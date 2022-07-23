a62b70c7a7f500cfef190d03b32c5b6b7c61b2ab
ManuelP2 <manuelperezp22@hotmail.com>
2022-07-23 11:24:58 -0400

    footer listo

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./footer.scss";
+import Rectangle7 from "../../assets/Rectangle7.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="enterprise__footer section__padding">
+        <div className="enterprise__footer-container">
+          <div className="enterprise__footer-container-inner">
+            <img src={Rectangle7} alt="rectangle" />
+            <p>Company Name</p>
+          </div>
+          <div className="enterprise__footer-container-p">
+            <p>Company, 2022 © All Rights Reserved</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
